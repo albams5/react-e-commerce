6eb91e99f1e0d50969207decbdf91146ed3d69af
albams5 <albams51289@gmail.com>
2024-06-26 14:00:21 +0200

    fix: UserContext import route

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Home } from "../pages/Home";
+import { Home } from "../pages/home";
 import { ShoppingCart } from "../pages/ShoppingCart";
 import { ProductPage } from "../pages/ProductPage";
 import Login from "../pages/Login";
